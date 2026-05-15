74beeff8f62256c0b7314352d7800c13310be6d4
poornima pappu <poornimapappu18@gmail.com>
2026-05-15 15:30:39 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,38 @@
+
+any="python is a launguage"
+print(any[::-2])
+
+1.program to convert 24h clock into nrml clock
+
+'''
+time_ =input ("enter 24 hours time: ")
+parts_=time_.split(":")
+hour_= int(parts[0])
+min_= int(parts_[1])
+print(f"{time_} is converted into{hour_ -12}:{min_} pm")
+
+
+list
+list is a collection of different data tyoes and represented in [] and seperated by ,
+ --->it is mutable
+ 
+any=[1,"python",[1,2]]
+print(any)
+
+     
+any=[1,"python",[1,2[34,"this is python 3rd class",78],"python is a launguage",89],34,[3,4]]
+print(any
+
+append()
+this method s used to add new item into list, and it will in the lastindex position
+syntax-->variable_name.append(item)
+
+
+any=[1,2,3]
+any.append(6)
+any.append([20,90])
+print(any)
+
+EXTEND
+-->this method is used to add itterable into list,an…
+      
